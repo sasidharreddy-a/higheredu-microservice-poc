6bf669f881455ab992bd2671e3aaf84494372c48
Sasidhar Reddy <sasidharreddya@kpmg.com>
2025-01-21 19:21:31 +0530

    Integrating all services with Eureka server

--- higheredu_api/course-service/src/main/java/com/higheredu_api/course/CourseServiceApplication.java
+++ higheredu_api/course-service/src/main/java/com/higheredu_api/course/CourseServiceApplication.java
@@ -1,9 +1,11 @@
+package com.higheredu_api.course;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class CourseServiceApplication {
 
 	public static void main(String[] args) {
--- higheredu_api/grading-service/src/main/java/com/higheredu_api/grading_service/GradingServiceApplication.java
+++ higheredu_api/grading-service/src/main/java/com/higheredu_api/grading_service/GradingServiceApplication.java
@@ -2,8 +2,10 @@ package com.higheredu_api.grading_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GradingServiceApplication {
 
 	public static void main(String[] args) {
