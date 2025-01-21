76f0e13af5ae1d4e39e746a8ad0e1dafde7bba6b
Sasidhar Reddy <sasidharreddya@kpmg.com>
2025-01-21 14:42:35 +0530

    Eureka client  Integration changes

--- higheredu_api/course-service/src/main/java/com/higheredu_api/course/CourseServiceApplication.java
+++ higheredu_api/course-service/src/main/java/com/higheredu_api/course/CourseServiceApplication.java
@@ -1,7 +1,6 @@
-package com.higheredu_api.course;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
